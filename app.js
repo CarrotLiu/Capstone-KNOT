22932857325d6e7a6058b0db18e83a4043fb5e64
CarrotLiu <79093800+CarrotLiu@users.noreply.github.com>
2023-04-12 14:26:14 +0800

    add image button trial

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(session(sessionOptions));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("assets"));
 
 // passport setup
 app.use(passport.initialize());
